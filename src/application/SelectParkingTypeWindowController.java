799b4ed967af90b53d6be454b6a2ad1401c9ef6e
sergeydus <gogosergey@gmail.com>
2018-06-05 20:02:05 +0300

    eyboy

--- src/application/SelectParkingTypeWindowController.java
+++ src/application/SelectParkingTypeWindowController.java
@@ -65,7 +65,7 @@ public class SelectParkingTypeWindowController {
         	stage.show();
     		Stage stage2 = (Stage) OneTimeParkingButton .getScene().getWindow();
         	stage2.close();
-    		
+    		//gayboy
         	}
         	catch(IOException e){
         		e.printStackTrace();
