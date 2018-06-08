f94f3f17197e456c8a2af744fb9cd3f4262c8905
sergeydus <gogosergey@gmail.com>
2018-06-08 15:28:13 +0300

    updated employee

--- src/application/EmployeeWindowController.java
+++ src/application/EmployeeWindowController.java
@@ -1,15 +1,15 @@
 /**
- * Sample Skeleton for "EmployeeWindow.fxml" Controller Class
- * You can copy and paste this code into your favorite IDE
- **/
+ * Sample Skeleton for 'EmployeeWindow.fxml' Controller Class
+ */
 
 package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.Button;
 
 public class EmployeeWindowController {
 
@@ -19,12 +19,35 @@ public class EmployeeWindowController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="VehicleLeaveButton"
+    private Button VehicleLeaveButton; // Value injected by FXMLLoader
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML // fx:id="ParkingCancelButton"
+    private Button ParkingCancelButton; // Value injected by FXMLLoader
 
-        // Initialize your logic here: all @FXML variables will have been injected
+    @FXML // fx:id="VehicleEnterButton"
+    private Button VehicleEnterButton; // Value injected by FXMLLoader
+
+    @FXML
+    void VehicleEnterMethod(ActionEvent event) {
 
     }
 
+    @FXML
+    void VehicleLeaveMethod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ParkingCancelMethod(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert VehicleLeaveButton != null : "fx:id=\"VehicleLeaveButton\" was not injected: check your FXML file 'EmployeeWindow.fxml'.";
+        assert ParkingCancelButton != null : "fx:id=\"ParkingCancelButton\" was not injected: check your FXML file 'EmployeeWindow.fxml'.";
+        assert VehicleEnterButton != null : "fx:id=\"VehicleEnterButton\" was not injected: check your FXML file 'EmployeeWindow.fxml'.";
+
+    }
 }
